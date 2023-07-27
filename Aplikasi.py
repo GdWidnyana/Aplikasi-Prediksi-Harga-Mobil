7314565cdbbaca1b6436ecd71659192a0ad66991
GdWidnyana <120539660+GdWidnyana@users.noreply.github.com>
2023-07-28 01:58:02 +0800

    Aplikasi.py

--- Aplikasi.py
+++ Aplikasi.py
@@ -8,7 +8,7 @@ st.title('Aplikasi Prediksi Harga Mobil Bekas')
 
 st.image('mobil.png', use_column_width=True)
 
-year = st.number_input('Input Tahun Keluaran Mobil')
+year = st.number_input('Input Tahun Keluaran Mobil', min_value=0, step=1, format="%d")
 mileage = st.number_input('Input Jarak Tempuh Mobil')
 tax = st.number_input('Input Pajak Mobil')
 mpg = st.number_input('Input Konsumsi BBM Mobil')
@@ -16,8 +16,11 @@ engineSize = st.number_input('Input Engine Size Mobil')
 
 predict = ' '
 
-def format_number_with_commas(number):
-    return "{:,.0f}".format(number).replace(",", ".")
+def format_number_with_dots(number):
+    # Convert the number to an integer to remove decimal digits
+    integer_value = int(number)
+    # Format the integer with dots as a thousands separator
+    return "{:,}".format(integer_value).replace(",", ".")
 
 if st.button('Prediksi Harga Mobil Bekas', key='predict_button'):
     if year == 0 or mileage == 0 or tax == 0 or mpg == 0 or engineSize == 0:
@@ -33,10 +36,10 @@ if st.button('Prediksi Harga Mobil Bekas', key='predict_button'):
         predict = model.predict([[year, mileage, tax, mpg, engineSize]])
         predicted_price_in_rupiah = predict[0] * 16741  # Assuming predict is a 1D array with a single value
 
-        st.write('Prediksi Harga Mobil Bekas dalam EURO adalah', predict[0])
-        st.write('Prediksi Harga Mobil Bekas dalam Rupiah adalah', predicted_price_in_rupiah)
+        st.write('Prediksi Harga Mobil Bekas dalam EURO adalah', format_number_with_dots(predict[0]))
+        st.write('Prediksi Harga Mobil Bekas dalam Rupiah adalah', format_number_with_dots(predicted_price_in_rupiah))
 
-        formatted_price = format_number_with_commas(predicted_price_in_rupiah)
+        formatted_price = format_number_with_dots(predicted_price_in_rupiah)
         st.success(f"Kesimpulan: Harga mobil bekas berdasarkan data di atas adalah Rp {formatted_price}.")
         st.snow()
 
@@ -47,9 +50,9 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://wallpapertag.com/wallpaper/middle/8/0/1/350871-website-background-1920x1200-high-resolution.jpg");
+             background-image: url("https://cutewallpaper.org/27/blue-black-hd-high-res-wallpaper/170426645.jpg");
              background-attachment: fixed;
-             background-size: cover
+             background-size: cover;
          }}
          </style>
          """,
